7fdf84f995cf862c7c020a725a198b1d71fec6df
sanket mane <sanketmmane@gmail.com>
2024-01-01 23:25:28 +0530

    datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountid = 393232
+let accountemail = "sanket@gmail.com"
+var accountpassword = "1234"
+accountcity = "pune"
+let accountstate
+
+// accountid = 2 // not allowed
+accountemail = "sndwu@gmail"
+accountpassword = 122
+accountcity = "mumbai"
+
+// never to use var bcz of variable scope and function scope
+
+console.log(accountid);
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+// alert("hello") we are using node js not browser
+
+let name = "sanket"
+let age = 20
+let isloggedin = false
+let state;
+
+//premetive
+//number => to power 53
+//bigint 
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//symbol => unique
+
+//nonpremetive
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);// object

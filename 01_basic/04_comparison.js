1f2ee2fee43ac48049d68bb3a901539ddc358210
sanket mane <sanketmmane@gmail.com>
2024-01-02 11:30:13 +0530

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);//true
+// console.log(2 >= 1);//true
+// console.log(2 < 1);//false
+// console.log(2 == 1);//false
+// console.log(2 != 1);//true
+
+
+// console.log("2" > 1);//true - automatically convert number
+// console.log("0" > 1);//false 
+
+// console.log(null > 0);//null = empty value false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true - it convert null to number
+
+// console.log(undefined == 0);//false
+// console.log(undefined >= 0);//false
+// console.log(undefined < 0);//false
+
+// === it compare strictly datatype and value
+// == it only compare value
+console.log("2" === 2);//false
+console.log("2" == 2);//true - it convert to number

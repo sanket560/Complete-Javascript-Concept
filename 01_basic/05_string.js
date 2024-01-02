0680e4caa487a52fac7c72232ea9d8f1eba2a317
sanket mane <sanketmmane@gmail.com>
2024-01-02 12:07:32 +0530

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,10 @@
+// const name = "sanket"
+// const repoCount  = 50
+// console.log(name+repoCount); // outdated 
+//new method string interpolation 
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const myName = new String("sanket mane")
+console.log(myName.toUpperCase());//it wont change the original value
+console.log(myName.charAt(2));
+console.log(myName.indexOf("n"));

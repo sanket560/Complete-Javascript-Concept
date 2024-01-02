fb6fbe933d75e6910f5fe7e89a68b8e4fab534cd
sanket mane <sanketmmane@gmail.com>
2024-01-02 19:13:52 +0530

    date and time

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,14 @@
+let myDate = new Date()
+// console.log(myDate);
+//2024-01-02T13:31:06.338Z
+// console.log(myDate.toString());
+//Tue Jan 02 2024 19:01:06 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString());
+//Tue Jan 02 2024
+// console.log(myDate.toLocaleString());
+//2/1/2024, 7:01:06 pm
+// console.log(myDate.getTime());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMonth());
+// console.log(myDate.getHours());
+// console.log(myDate.getDate());

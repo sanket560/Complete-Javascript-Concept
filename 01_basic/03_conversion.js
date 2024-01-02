ab5e124c51c20758eb8ad9182c8431a0d3a7f829
sanket mane <sanketmmane@gmail.com>
2024-01-02 11:14:14 +0530

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,26 @@
+// let score = "33"
+// console.log(typeof score);//string
+
+// let valueInNumber = Number(score)//conversion
+// console.log(typeof valueInNumber);//number
+
+// let score2 = "33abc"
+// console.log(typeof score2)//string
+// let newValue = Number(score2)
+// console.log(typeof newValue)//number
+// console.log(newValue)//NaN not a number
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); //true
+
+// 1 => true 0 => false
+// "" => false
+//"sanket" => true

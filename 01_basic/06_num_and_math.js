f2edb0db3e02efa1832f247bc9745f95e4371b2b
sanket mane <sanketmmane@gmail.com>
2024-01-02 18:56:47 +0530

    number and math

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,27 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+// const hundreds  = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++ math +++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-2)); // gives same number in postive way
+// console.log(Math.round(4.6));
+// console.log(Math.min(4,2,1,3));
+// console.log(Math.max(4,2,1,3));
+
+// console.log(Math.random()); // return number from 0 to 1
+// console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random() * 10 + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+

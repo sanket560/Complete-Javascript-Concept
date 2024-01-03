d7a42d557b650c71dd09c0f9a1622cc43f6c69f9
sanket mane <sanketmmane@gmail.com>
2024-01-03 11:30:49 +0530

    object

---
+++ 02_basic/03_object.js
@@ -0,0 +1,26 @@
+// object can be created by two methods 
+// 1 . literals
+// 2 . constructor
+// singleton : when u create object from constructor makes singleton object
+
+// object literals
+const mySymbol = Symbol("key1")
+const jsUser = {
+    [mySymbol]:"mykey1",
+    name: "sanket",
+    age:20,
+    location:"pune",
+    email:"sanket@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["monday","tuesday"]
+}
+// console.log(jsUser["name"]);
+// console.log(jsUser[mySymbol]);
+// jsUser.email = "sanket@123"
+// Object.freeze(jsUser)
+// jsUser.email = "sanket@gmail.com"
+// console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("hello js user");
+}

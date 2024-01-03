e0471000514601bb988f0f4218b3dfacb5fe2947
sanket mane <sanketmmane@gmail.com>
2024-01-03 19:08:25 +0530

    function

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -8,12 +8,29 @@ function addTwoNumber(num1 , num2){//parameter
 }
 // addTwoNumber(4,2)//arguments
 
-function loginUser(username) {
-    if(!username){
-        console.log("error");
-    }else{
-        console.log(`${username} is login`);
-    }
+// function loginUser(username) {
+//     if(!username){
+//         console.log("error");
+//     }else{
+//         console.log(`${username} is login`);
+//     }
+// }
+
+// console.log(loginUser(""));
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(10,30,20));
+
+const user = {
+    username : "sanket",
+    age : 20
 }
 
-console.log(loginUser(""));
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and age is ${anyobject.age}`);
+}
+
+handleObject(user)

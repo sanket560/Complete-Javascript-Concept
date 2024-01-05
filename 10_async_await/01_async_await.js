4bc66daaceb18aefd784b5b977b1c6f5a5bce78b
sanket mane <sanketmmane@gmail.com>
2024-01-06 00:56:31 +0530

    async await

---
+++ 10_async_await/01_async_await.js
@@ -0,0 +1,20 @@
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+async function getUserData(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+getUserData()
